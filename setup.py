b95be86bc0f621ec5b9a456422eaedefacbfbcae
Daltz333 <daltzsmith@gmail.com>
2020-12-24 14:34:21 -0500

    Add PyPi Publishing Information

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 import re
-
+import subprocess
 from setuptools import setup
+
+try:
+    ret = subprocess.check_output(
+        "git describe --tags --abbrev=0",
+        shell=True,
+    )
+    version = ret.decode("utf-8").strip()
+except:
+    version = "master"
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
 
 setup(
     name="sphinx-lynx",
     version="0.1",
+    author="Vasista Vovveti",
+    author_email="vasistavovveti@gmail.com",
+    description="doc8 linter for frc-docs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wpilibsuite/sphinx-lynx",
     packages=["sphinx-lynx"],
     entry_points={
         "doc8.extension.check": [
@@ -18,5 +36,23 @@ setup(
         "more_itertools>=8.6.0",
         "doc8>=0.8.1",
         "docutils>=0.16",
+    ],
+    classifiers=[
+        "Environment :: Plugins",
+        "Environment :: Web Environment",
+        "Framework :: Sphinx :: Extension",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
     ],
 )
